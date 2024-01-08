d5c130aefdce28898b3afe69d77ce55534a9873a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-08 20:31:44 +0200

    Add resourcesful route for edit and update action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
 
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 
 end
