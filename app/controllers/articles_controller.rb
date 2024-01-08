2f3c8583bd3208b94b645a7877010038f6c7c0b6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-08 20:48:16 +0200

    Define edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,21 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def article_params
