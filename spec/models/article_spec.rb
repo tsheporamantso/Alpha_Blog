c582e598652eecd8b91c1af7e094b32521d3278f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-10 14:39:29 +0200

    Add spec test cases for article validation model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,8 +7,35 @@ RSpec.describe Article, type: :model do
 
   describe "article attributes to be present" do
     it "title should be present" do
-      subject.title = 'This is some random text for title'
-      expect(subject).to be_valid
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "description shouldbe present" do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe "title and description text length" do
+    it "title should not be too long" do
+      subject.title = "t" * 101
+      expect(subject).to_not be_valid
+    end
+
+    it "title shouldnot be too short" do
+      subject.title = "a"
+      expect(subject).to_not be_valid
+    end
+
+    it "description not to be too long" do
+      subject.description = "d" * 301
+      expect(subject).to_not be_valid
+    end
+
+    it "description not to be too short" do
+      subject.description = "d"
+      expect(subject).to_not be_valid
     end
   end
 end
